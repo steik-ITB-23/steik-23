1b117d8ead05c0b2d31182f31661e33e2e7ab117
yonatan-nyo <yonatan.njoto@gmail.com>
2024-02-25 00:33:18 +0700

    added route with all catch nextauth

---
+++ src/pages/api/auth/[nextauth]/route.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+  pages: {
+    signIn: "/login",
+  },
+});
+
+export { handler as GET, handler as POST };
