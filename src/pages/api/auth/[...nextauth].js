9e50ce7bb422af3e5466da2daa56028708d1b3a6
yonatan-nyo <yonatan.njoto@gmail.com>
2024-02-25 09:38:47 +0700

    added pages and callbacks

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      return !!user;
+    },
+    async session({ session, token, user }) {
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/auth/signin",
+    verifyRequest: "/auth/verify-request",
+    error: "/auth/error",
+  },
+};
+
+export default NextAuth(authOptions);
