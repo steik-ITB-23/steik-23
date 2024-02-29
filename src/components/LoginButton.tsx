7dd94c766b1b86202b788bbb01aa3cb3834383d1
yonatan-nyo <yonatan.njoto@gmail.com>
2024-02-29 22:33:05 +0700

    fixed sign in color

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -88,7 +88,7 @@ export default function LoginButton() {
   }
   return (
     <>
-      <button onClick={() => signIn()} className="hover:brightness-95 bg-slate-50 px-2 py-1 rounded-md">
+      <button onClick={() => signIn()} className="hover:brightness-95 bg-slate-50 px-2 py-1 rounded-md text-slate-900">
         Sign in
       </button>
     </>
