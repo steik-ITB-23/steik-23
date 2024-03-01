70d0acbd973ce767067300fc1a2bf4f4d5e562d6
yonatan-nyo <yonatan.njoto@gmail.com>
2024-03-01 10:47:17 +0700

    remove redirect

--- src/pages/auth/signIn/index.tsx
+++ src/pages/auth/signIn/index.tsx
@@ -63,14 +63,14 @@ export default function SignIn({ providers }: InferGetServerSidePropsType<typeof
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getServerSession(context.req, context.res, authOptions);
+  // const session = await getServerSession(context.req, context.res, authOptions);
 
   // If the user is already logged in, redirect.
   // Note: Make sure not to redirect to the same page
   // To avoid an infinite loop!
-  if (session) {
-    return { redirect: { destination: "/" } };
-  }
+  // if (session) {
+  //   return { redirect: { destination: "/" } };
+  // }
 
   const providers = await getProviders();
 
