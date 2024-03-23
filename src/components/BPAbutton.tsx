bb5ccfada67033ebd68e26e53f59e0b2267daa06
Lint Action <lint-action@samuelmeuli.com>
2024-03-23 09:30:55 +0000

    Fix code style issues with Prettier

--- src/components/BPAbutton.tsx
+++ src/components/BPAbutton.tsx
@@ -5,10 +5,17 @@ import { getSlug } from "@/lib/utils/stringManipulation";
 
 const BPAbutton = ({ data }: { data: bpaType }) => {
   return (
-    <Link href={`/tentang-bpa/#${getSlug(data.name)}`} className="border-2 rounded-full border-slate-300 h-fit px-1 py-0.5">
+    <Link
+      href={`/tentang-bpa/#${getSlug(data.name)}`}
+      className="border-2 rounded-full border-slate-300 h-fit px-1 py-0.5"
+    >
       <p className="button">
         <span className="text-slate-500">{data.name}</span>{" "}
-        {data.members.length > 1 && <span className="font-bold text-slate-700">{data.members.length}</span>}
+        {data.members.length > 1 && (
+          <span className="font-bold text-slate-700">
+            {data.members.length}
+          </span>
+        )}
       </p>
     </Link>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,7 +24,10 @@ const outfit = Outfit({
   variable: "--font-outfit",
 });
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   const router = useRouter();
   const [scrolledOneThirdvw, setScrolledOneThirdvw] = useState(false);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
@@ -73,9 +76,11 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
           router.pathname === "/" && !scrolledOneThirdvw
             ? "bg-gradient-to-t from-black to-[#242828] text-slate-100"
             : "bg-slate-100 text-slate-900"
-        }`}>
+        }`}
+      >
         <main
-          className={`flex min-h-screen flex-col items-center justify-between mx-auto overflow-x-clip ${poppins.variable} ${inter.variable} ${outfit.variable}`}>
+          className={`flex min-h-screen flex-col items-center justify-between mx-auto overflow-x-clip ${poppins.variable} ${inter.variable} ${outfit.variable}`}
+        >
           <Component {...pageProps} />
         </main>
       </div>
