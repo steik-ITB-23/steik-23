edbae58dab09adb7a7302eb3834aa7dc00d30690
yonatan-nyo <yonatan.njoto@gmail.com>
2024-04-10 12:09:45 +0700

    changed to app router

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-const NEXT_PUBLIC_CLERK_SIGN_IN_URL = process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL || "auth/sign-in";
-const NEXT_PUBLIC_CLERK_SIGN_UP_URL = process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL || "auth/sign-up";
+const NEXT_PUBLIC_CLERK_SIGN_IN_URL = process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL || "/auth/sign-in";
+const NEXT_PUBLIC_CLERK_SIGN_UP_URL = process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL || "/auth/sign-up";
 
 export default authMiddleware({
   apiRoutes: [NEXT_PUBLIC_CLERK_SIGN_IN_URL, NEXT_PUBLIC_CLERK_SIGN_UP_URL],
