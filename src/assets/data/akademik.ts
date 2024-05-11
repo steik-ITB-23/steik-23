739657ef81dfdc6fd52bd940a13184dcfb29a668
Dave <d06163606@gmail.com>
2024-05-11 19:48:50 +0700

    bpa akademik page

---
+++ src/assets/data/akademik.ts
@@ -0,0 +1,104 @@
+import { AkademikType } from "@/components/AkademikCard";
+
+//dapat ditulis export const AkademikList: {name:string;}[]
+export const AkademikList: AkademikType[] = [
+  {
+    name: "Catatan Bab 7 Persamaan DIfferensial Orde 1",
+  },
+  {
+    name: "Catatan Bab 7 Persamaan DIfferensial Orde 1",
+  },
+  {
+    name: "Catatan Bab 7 Persamaan DIfferensial Orde 1",
+  },
+  {
+    name: "Catatan Bab 7 Persamaan DIfferensial Orde 1",
+  },
+  {
+    name: "Catatan Bab 7 Persamaan DIfferensial Orde 1",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest",
+  },
+  {
+    name: " test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+  {
+    name: "test",
+  },
+];
